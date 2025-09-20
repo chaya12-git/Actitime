c9443f4cc005c4be1577fd0942f9fd87588abd5d
chaya12-git <chaya860515@gmail.com>
2025-09-20 12:57:35 +0530

    Added a new Test Script: Create Report by TE2

--- Actitime/src/com/actitime/testscript/ReportsModule.java
+++ Actitime/src/com/actitime/testscript/ReportsModule.java
@@ -1,5 +1,10 @@
 package com.actitime.testscript;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 public class ReportsModule {
-
+  @Test
+public void createReport(){
+  Reporter.log("createReport",true);
+}
 }
